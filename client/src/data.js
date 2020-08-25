bd0699b4a1e4c2b0ef5e95aa1abfb25addbb7386
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-08-25 14:29:47 -0400

    CREATED react-app for client and PASTED template index.html and style.css

---
+++ client/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            name: 'Mini Handheld Portable Fan',
+            category: 'Summer',
+            image: 'fan1.jpg',
+            price: 13.99,
+            brand: 'IPOW',
+            rating: 4.8,
+            numReviews: 625,
+        },
+        {
+            name: 'Mini Flexible Tripod Clip On Fan',
+            category: 'Summer',
+            image: 'fan2.jpg',
+            price: 19.99,
+            brand: 'Amacool',
+            rating: 4.9,
+            numReviews: 2589,
+        },
+        {
+            name: 'Portable Neck Fan',
+            category: 'Summer',
+            image: 'fan3.jpg',
+            price: 19.99,
+            brand: 'IPOW',
+            rating: 4.5,
+            numReviews: 1623,
+        },
+        {
+            name: 'Lady Handheld Portable Fan',
+            category: 'Summer',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/51kxZt06SUL._AC_SL1000_.jpg',
+            price: 13.99,
+            brand: 'IPOW',
+            rating: 4.4,
+            numReviews: 869,
+        },
+    ]
+}
